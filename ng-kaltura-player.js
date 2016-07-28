f6d0ca4e02ba0eb2ef6ee3e36c7f462aeccddba9
Andrew Lively <livelyar@ucmail.uc.edu>
2016-07-28 14:39:04 -0400

    No need to load the script twice

--- ng-kaltura-player.js
+++ ng-kaltura-player.js
@@ -8,7 +8,7 @@ angular
         template: `<div id="kaltura_player_{{ id }}" style="width:{{ width }}; height:{{ height }}; background-color: black"></div>`,
         scope: { video: `=`, height: `@`, width: `@` },
         controller: function($scope) {
-          if (document.getElementById(`kalturaLib`) === null){
+          if (document.getElementById(`kalturaLib`) === null && !window.kWidget){
             const s = document.createElement(`script`);
             s.src = `http://cdnapi.kaltura.com/p/${ $scope.video.pid }/sp/${ $scope.video.pid }00/embedIframeJs/uiconf_id/${ $scope.video.uiconfid }/partner_id/${ $scope.video.pid }`;
             s.id = `kalturaLib`;
@@ -17,24 +17,26 @@ angular
           }
 
           const intervalID = setInterval(function(){
-            if (typeof window.kWidget !== `undefined`){
-              clearInterval(intervalID);
-
-              const target = $scope.id ? `kaltura_player_${ $scope.id }` : `kaltura_player_`;
-              const flashvars = $scope.video.flashvars ? JSON.parse($scope.video.flashvars) : {};
-
-              window.kWidget.embed({
-                "targetId": target,
-                "wid": `_${ $scope.video.pid }`,
-                "uiconf_id": $scope.video.uiconfid,
-                "flashvars": flashvars,
-                "cache_st": Math.random(),
-                "entry_id": $scope.video.entryid,
-                "readyCallback": function(playerID) {
-                  $scope.kdp = document.getElementById(playerID);
-                }
-              });
+            if (typeof window.kWidget === `undefined`) {
+              return;
             }
+
+            clearInterval(intervalID);
+
+            const target = $scope.id ? `kaltura_player_${ $scope.id }` : `kaltura_player_`;
+            const flashvars = $scope.video.flashvars ? JSON.parse($scope.video.flashvars) : {};
+
+            window.kWidget.embed({
+              "targetId": target,
+              "wid": `_${ $scope.video.pid }`,
+              "uiconf_id": $scope.video.uiconfid,
+              "flashvars": flashvars,
+              "cache_st": Math.random(),
+              "entry_id": $scope.video.entryid,
+              "readyCallback": function(playerID) {
+                $scope.kdp = document.getElementById(playerID);
+              }
+            });
           }, 50);
         }
       };
